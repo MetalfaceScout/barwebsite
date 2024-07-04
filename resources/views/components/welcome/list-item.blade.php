2fe489767ef1867b71801d0c0ef6715b59105385
MetalfaceScout <JLsuckers@gmail.com>
2024-07-03 23:31:26 -0600

    Mess with themes

--- resources/views/components/welcome/list-item.blade.php
+++ resources/views/components/welcome/list-item.blade.php
@@ -1,3 +1,5 @@
-<div class="sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-zinc-900 dark:text-gray-300">
+<div class="sm:justify-center items-center pt-6 sm:pt-0  bg-gray-100 dark:bg-zinc-900">
+    <p class="dark:text-gray-300 text-white">
         {{ $slot }}
+    </p>
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,10 @@
 <x-guest-layout>
-    <div class="flex flex-row gap-10 dark:bg-zinc-900 m-10">
+    <div>
+
+    </div>
+    <div class="flex flex-row gap-10 bg-zinc-100 dark:bg-zinc-900 p-10">
         <x-welcome.list-item>
             Charge your event!
         </x-welcome.list-item>
-        
-        <h2>
-            Bring your own drinks for your own type of fun!
-        </h2>
-        <h3>
-            Book your rental now!
-        </h3>
     </div>
 </x-guest-layout>
