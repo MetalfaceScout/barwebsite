eeaccd354919b9c69cf67918dc2ce1380c084ba7
MetalfaceScout <logan.mathis@utahtech.edu>
2024-07-03 22:41:03 -0600

    first commit

---
+++ resources/views/components/welcome/list-item.blade.php
@@ -0,0 +1,3 @@
+<div class="sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-zinc-900 dark:text-gray-300">
+        {{ $slot }}
+</div>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<x-guest-layout>
+    <div class="flex flex-row gap-10 dark:bg-zinc-900 m-10">
+        <x-welcome.list-item>
+            Charge your event!
+        </x-welcome.list-item>
+        
+        <h2>
+            Bring your own drinks for your own type of fun!
+        </h2>
+        <h3>
+            Book your rental now!
+        </h3>
+    </div>
+</x-guest-layout>
